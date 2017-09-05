12ac5043fa536fa1ea597f01ff12b62f31ea4a95
Gustavo Cid Ornelas <gustavocidornelas@gmail.com>
2017-09-05 16:33:47 -0300

    Alamouti combiner (2x1)

---
+++ src/+modem/@InnerReceiver/combiner.m
@@ -0,0 +1,36 @@
+function [ rxSignalOut ] = combiner( rxSignal, channelInFreq )
+% Alamouti combining scheme for one receiving antenna and 2 transmitting
+% antennas.
+
+%% Parameters (olhar fillFrame e escolher o mesmo)
+space_time = 0; 
+space_frequency = 1;
+
+%% RX signal after the combiner
+rxSignalOut =zeros(size(rxSignal));
+
+if(space_time == 1 && space_frequency == 0)
+    %% For STBC
+    for i = 1:1:size(rxSignal,1) % Lines 
+        for j = 1:2:size(rxSignal,2)-1 % Columns 
+            rxSignalOut(i,j) = conj(channelInFreq(i,j,:, 1))*rxSignal(i,j) + ...
+                        channelInFreq(i,j,:, 2)*conj(rxSignal(i,j+1));
+            rxSignalOut(i,j+1) = conj(channelInFreq(i,j,:, 2))*rxSignal(i,j) - ...
+                          channelInFreq(i,j,:, 1)*conj(rxSignal(i,j+1));
+        end 
+    end
+elseif(space_time == 0 && space_frequency == 1)
+    %% For SFBC
+    for j = 1:1:size(rxSignal,2) % Columns
+            for i = 1:2:size(rxSignal,1)-1 % Lines
+                rxSignalOut(i,j) = conj(channelInFreq(i,j,:, 1))*rxSignal(i,j) + ...
+                           channelInFreq(i,j,:, 2)*conj(rxSignal(i+1,j));
+                rxSignalOut(i+1,j) =  conj(channelInFreq(i,j,:, 2))*rxSignal(i,j) - ...
+                           channelInFreq(i,j,:, 1)*conj(rxSignal(i+1,j));
+                display(i)
+                display(i+1)
+            end
+    end
+else
+    display('Error. Choose either STBC or SFBC.')
+end
