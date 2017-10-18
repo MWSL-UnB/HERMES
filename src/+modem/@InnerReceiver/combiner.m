0a639a1ddfeae779ec3e7614608d91de0d51a551
Gustavo Cid Ornelas <gustavocidornelas@gmail.com>
2017-10-18 08:51:34 -0200

    Adjusted SFBC to work for any number of Rx antennas.

--- src/+modem/@InnerReceiver/combiner.m
+++ src/+modem/@InnerReceiver/combiner.m
@@ -9,7 +9,7 @@ space_frequency = 0;
 numberOfRxAntennas = size(rxSignal, 3);
 
 %% RX signal after the combiner
-rxSignalOut =zeros(size(rxSignal(:,:,1)));
+rxSignalOut = zeros(size(rxSignal(:,:,1)));
 
 if(space_time == 1 && space_frequency == 0)
     %% For STBC
@@ -36,10 +36,22 @@ elseif(space_time == 0 && space_frequency == 1)
     %% For SFBC
     for j = 1:1:size(rxSignal,2) % Columns
             for i = 1:2:size(rxSignal,1)-1 % Lines
-                rxSignalOut(i,j) = conj(channelInFreq(i,j,:, 1))*rxSignal(i,j) + ...
-                           channelInFreq(i,j,:, 2)*conj(rxSignal(i+1,j));
-                rxSignalOut(i+1,j) =  conj(channelInFreq(i,j,:, 2))*rxSignal(i,j) - ...
-                           channelInFreq(i,j,:, 1)*conj(rxSignal(i+1,j));
+                for rxAntCount = 1:1:numberOfRxAntennas
+                    
+                    currentCombiner1 = conj(channelInFreq(i,j,rxAntCount, 1))*rxSignal(i,j, rxAntCount) + ...
+                                        channelInFreq(i,j,rxAntCount, 2)*conj(rxSignal(i+1,j, rxAntCount));
+                                    
+                    currentCombiner2 = conj(channelInFreq(i,j,rxAntCount, 2))*rxSignal(i,j, rxAntCount) - ...
+                                        channelInFreq(i,j,rxAntCount, 1)*conj(rxSignal(i+1,j, rxAntCount));
+                                    
+                    rxSignalOut(i,j) = rxSignalOut(i,j) + currentCombiner1;
+                    rxSignalOut(i+1,j) = rxSignalOut(i+1,j) + currentCombiner2;
+                        
+                end              
+                %rxSignalOut(i,j) = conj(channelInFreq(i,j,:, 1))*rxSignal(i,j) + ...
+                %           channelInFreq(i,j,:, 2)*conj(rxSignal(i+1,j));
+                %rxSignalOut(i+1,j) =  conj(channelInFreq(i,j,:, 2))*rxSignal(i,j) - ...
+                %           channelInFreq(i,j,:, 1)*conj(rxSignal(i+1,j));
             end
     end
 else
